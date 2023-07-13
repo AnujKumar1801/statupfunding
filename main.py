e48dd5543fa61dce05c0b5dd8bb7da791c837fc6
AnujKumar1801 <107303222+AnujKumar1801@users.noreply.github.com>
2023-07-13 05:58:36 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# from flask import Flask
+import streamlit as st
+# st.set_option('server.host', '0.0.0.0')
+
+st.title('Startup Dashboard')
+# app = Flask('app')
+
+# @app.route('/')
+# def hello_world():
+#   return 'Hello, World!'
+
+# app.run(host='0.0.0.0', port=8080)
